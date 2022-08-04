4c5bedfcef232ce8fe7cc79fde2208f2a33b5e51
CUIYX <CUIYX@GMC16381.ad.goodmanmfg.com>
2022-08-04 14:17:38 -0500

    2

--- Digipass/src/DPManage/ShiftCatalog.java
+++ Digipass/src/DPManage/ShiftCatalog.java
@@ -190,6 +190,7 @@ public class ShiftCatalog {
 				
 		//Confirm TestYoo catalog is created successfully.
 				driver.navigate().refresh();
+				Thread.sleep(3000);
 				ele1 = driver.findElement(By.xpath("//*[text()='In Use?']"));
 				parent = ele1.findElement(By.xpath("./.."));
 				child = parent.findElement(By.xpath("./div[2]/div[1]/div[1]/span[1]"));
@@ -237,6 +238,7 @@ public class ShiftCatalog {
 				
 				// check Active, and save changes.
 				driver.navigate().refresh();
+				Thread.sleep(3000);
 				ele1 = driver.findElement(By.xpath("//*[text()='In Use?']"));
 				parent = ele1.findElement(By.xpath("./.."));
 				child = parent.findElement(By.xpath("./div[2]/div[1]/div[1]/span[1]"));
@@ -283,6 +285,7 @@ public class ShiftCatalog {
 				
 				//Confirm TestYoo-Copy is created.
 				driver.navigate().refresh();
+				Thread.sleep(3000);
 				ele1 = driver.findElement(By.xpath("//*[text()='In Use?']"));
 				parent = ele1.findElement(By.xpath("./.."));
 				child = parent.findElement(By.xpath("./div[2]/div[1]/div[1]/span[1]"));
@@ -328,6 +331,7 @@ public class ShiftCatalog {
 				
 				//confirm can't find TestYoo-Copy
 				driver.navigate().refresh();
+				Thread.sleep(3000);
 				ele1 = driver.findElement(By.xpath("//*[text()='In Use?']"));
 				parent = ele1.findElement(By.xpath("./.."));
 				child = parent.findElement(By.xpath("./div[2]/div[1]/div[1]/span[1]"));
